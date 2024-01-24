882270ad6cb163a92bf318138b791b0cd3aed502
DanielEromonsele <greatereromonsele01@gmail.com>
2024-01-24 16:09:02 +0100

    deleted blogs and updated blog router

--- mainApp.ts
+++ mainApp.ts
@@ -1,8 +1,11 @@
 import { Application, Request, Response } from "express";
 import user from "./router/userRouter";
+import blog from "./router/blogRouter";
+
 export const mainApp = (app: Application) => {
   try {
     app.use("/api", user);
+    app.use("/api", blog);
     app.get("/", (req: Request, res: Response) => {
       try {
         return res.status(200).json({
