e28ccafe20c9bd87489836f230f92a0627210844
Ayomide24500 <Ayomideadisa83@gmail.com>
2024-01-24 15:58:15 +0100

    update user router

--- router/userRouter.ts
+++ router/userRouter.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { createUser, getAllUser } from "../controller/userController";
+import {
+  createUser,
+  getAllUser,
+  signUser,
+  verifiedUser,
+} from "../controller/userController";
 
 const router: Router = Router();
 
 router.route("/create-user").post(createUser);
+router.route("/verify-user/:userID").patch(verifiedUser);
+router.route("/login-user").post(signUser);
 router.route("/get-All-user").get(getAllUser);
 router.route("/get-one-user/:userID").get(getAllUser);
 
